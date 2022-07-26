d988a9fd5ebadf5adbbe6d1606a96e7ac3504960
robsevcode <robsevcode@gmail.com>
2022-07-25 23:27:45 -0700

    Updated Experience page and some bug related to header

--- assets/js/generic.js
+++
@@ -1,5 +0,0 @@
-window.onload = function() {
-    document.getElementById("workTime").innerHTML = getWorkExperience("2021-11-01");
-    document.getElementById("totalWorkTime").innerHTML = getWorkExperience("2019-08-30");
-    alert("yo");
-  };
